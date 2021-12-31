d4f543f70dd1a51d57a5a5ccf52fba703c6e23ac
ZOG-WEB <79850764+ZOG-WEB@users.noreply.github.com>
2021-12-31 12:38:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+ var SpeechRecognition=window.webkitSpeechRecognition;
+
+ var recognition= new SpeechRecognition();
+ 
+ function start(){
+document.getElementById("textbox").innerHTML="";
+recognition.start();
+ }
+
+ recognition.onresult=function(event){
+ console.log(event);
+ var Content=event.results[0][0].transcript;
+ document.getElementById("textbox").innerHTML=Content;
+ console.log(Content);
+ if(Content=="take my selfie")
+ {
+     console.log("taking selfie");
+     speak();
+
+ }
+ }
+ function speak(){
+     var synth=window.speechSynthesis;
+     speak_data="taking your selfie in 5 seconds";
+     var utterthis=new SpeechSynthesisUtterance(speak_data);
+     synth.speak(utterthis);
+     Webcam.attach(camera);
+     setTimeout(function(){
+         takesnapshot();
+         save();
+     },5000);
+ }
+
+camera=document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+
+function takesnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("output").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+    
+}
+ function save(){
+     link=document.getElementById("link");
+     image=document.getElementById("selfie_image").src;
+     link.href=image;
+     link.click();
+
+ }
